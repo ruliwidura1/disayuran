aac4581b248b62425a70293e817e8629b362762c
ruliwidura1 <ruliroyanawidura@gmail.com>
2023-12-11 09:34:04 +0700

    Feat: Membuat we disayuran

--- app/view/front/tujuanpengiriman/home.php
+++ app/view/front/tujuanpengiriman/home.php
@@ -196,71 +196,7 @@
   </div>
   <div class="row">
     <div class="col">
-      <h4 class="fw-bolder">Berkah Jaya</h4>
-      <h6 class="fw-lighter" style="font-size:10px;">Dikirim dari: Caringin, Kab Bandung</h6>
-    </div>
-    <div class="col">
-      <p class="text-end text-success"><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-        <label for="vehicle1"></label><br></p>
-        <div class="ellipsis-container">
-          <p class="text-black-50 ukuran ellipsis-text"></p>
-        </div>
-        <div class="ellipsis-container">
-          <p class="ellipsis-text"></p>
-        </div>
-        <p class="text-danger"><b></b></p>
-      </div>
-    </div>
-    <div class="row text-center" style="padding-top:20px;">
-      <div class="col-md-12" style="border-bottom: 2px solid #f0f0f0; background-color:lightgrey;">
-      </div>
-    </div>
-    <br />
-
-    <div class="row">
-      <div class="col">
-        <img src="media/abuabu.png" class="img-fluid">
-        <div class="ellipsis-container">
-          <p class="text-black-50 ukuran ellipsis-text"></p>
-        </div>
-        <div class="ellipsis-container">
-          <p class="ellipsis-text"></p>
-        </div>
-        <p class="text-danger"><b></b></p>
-      </div>
-      <div class="col" style="padding-right: 43px;">
-        <h6 class="text-dark">Jeruk Ponkan Import</h6>
-        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-        <label for="vehicle1"></label><br>
-        <p class="text-secondary" style="font-size:12px;">Broken White, Medium(M)</p>
-        <div id="module-quantity-input" class="pdp-block module">
-          <div class="pdp-mod-product-info-section sku-quantity-selection">
-            <div class="section-content">
-              <div class="next-number-picker next-number-picker-inline">
-                <div class="next-number-picker-handler-wrap">
-                  <a unselectable="unselectable" class="next-number-picker-handler next-number-picker-handler-up">
-                    <span unselectable="unselectable" class="next-number-picker-handler-up-inner">
-                      <i class="next-icon next-icon-add next-medium">
-                      </i>
-                    </span>
-                  </a>
-                  <a unselectable="unselectable" class="next-number-picker-handler next-number-picker-handler-down next-number-picker-handler-down-disabled">
-                    <span unselectable="unselectable" class="next-number-picker-handler-down-inner">
-                      <i class="next-icon next-icon-minus next-icon-medium">
-                      </i>
-                    </span>
-                  </a>
-                </div>
-                <div class="next-number-picker-input-wrap">
-                  <span class="next-input next-input-single next-input-medium next-number-picker-input">
-                    <input type="number" step="1" min="1" mac="20" value="1" autocomplete="off" height="100%">
-                  </span>
-                </div>
-              </div>
-              <span class="quantity-content-default"></span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <h4 class="fw-bolder">Cara Bayar</h4>
     </div>
   </div>
+</div>
